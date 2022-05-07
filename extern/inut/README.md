aa3c82564f3969ddd07e2aeaee9a057bc5f08160
Abel Calluaud <abel0b@protonmail.com>
2022-05-07 18:49:18 +0200

    git subrepo clone git@github.com:abel0b/inut.git extern/inut
    subrepo:
      subdir:   "extern/inut"
      merged:   "9282563"
    upstream:
      origin:   "git@github.com:abel0b/inut.git"
      branch:   "master"
      commit:   "9282563"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "2f68596"

---
+++ extern/inut/README.md
@@ -0,0 +1,16 @@
+[![pipeline status](https://gitlab.com/abeliam/inut/badges/master/pipeline.svg)](https://gitlab.com/abeliam/inut/commits/master)
+# inut
+unit test framework
+
+## build
+```bash
+mkdir build
+cd build
+cmake ..
+make
+```
+
+run example
+```bash
+./example/example
+```
---
+++ extern/inut/src/test.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "array/array.h"
+#include "inut/test.h"
+#include "inut/test_report.h"
+#include <sys/time.h>
+
+struct TestSuite * root = NULL;
+
+struct Array * tests = NULL;
+int id_count = 0;
+
+struct TestResult
+pass () {
+    return (struct TestResult) {TEST_RESULT_PASS, NULL};
+}
+
+struct TestResult
+fail (char * diagnostic) {
+    return (struct TestResult) {TEST_RESULT_FAIL, diagnostic};
+}
+
+struct TestResult
+skip () {
+    return (struct TestResult) {TEST_RESULT_SKIP, NULL};
+}
+
+struct TestResult
+todo () {
+    return (struct TestResult) {TEST_RESULT_TODO, NULL};
+}
+
+void
+test_init () {
+    root = malloc (sizeof (struct TestSuite));
+    root->parent = root;
+    root->name = TEST_ROOT;
+    strcpy(root->complete_name, "\0");
+    root->test_suites = array_make();
+    root->test_cases = array_make();
+}
+
+struct TestSuite *
+test_suite(struct TestSuite * suite, char * name) {
+    if (root == NULL) {
+        test_init();
+    }
+
+    struct TestSuite * parent_suite = (suite == NULL)? root : suite;
+
+    struct TestSuite * new_suite = malloc (sizeof (struct TestSuite));
+    new_suite->parent = parent_suite;
+    new_suite->name = name;
+    strcpy(new_suite->complete_name, "\0");
+    new_suite->test_suites = array_make();
+    new_suite->test_cases = array_make();
+    array_push(parent_suite->test_suites, new_suite);
+    return new_suite;
+}
+
+struct TestCase *
+test_case(struct TestSuite * suite, char * description, struct TestResult (*test)()) {
+    struct TestSuite * parent_suite = (suite == NULL)? root : suite;
+
+    struct TestCase * test_case = malloc (sizeof (struct TestCase));
+    test_case->description = description;
+    test_case->test = test;
+    array_push (parent_suite->test_cases, test_case);
+    return test_case;
+}
+
+int test_run(int argc, char * argv[]) {
+    struct TestSuite * suite;
+    struct timeval t1, t2;
+
+    struct TestSuiteIterator * suite_it;
+    long seed = 0;
+    int opt;
+    int seed_arg = 0;
+
+    while((opt = getopt(argc, argv, "s:")) != -1) {
+        switch (opt) {
+            case 's':
+                seed_arg = 1;
+                seed = atol(optarg);
+                break;
+        }
+    }
+
+    if (!seed_arg) {
+        gettimeofday(&t1, NULL);
+        seed =  t1.tv_sec * (int)1e6 + t1.tv_usec;
+    }
+
+    for(suite_it = test_suite_it_make (root);
+        !test_suite_it_done (suite_it);
+        test_suite_it_next (suite_it)) {
+        suite = test_suite_it_get (suite_it);
+
+        if (strcmp(suite->name, TEST_ROOT) != 0) {
+            if (strcmp(suite->parent->name, TEST_ROOT) != 0) {
+                strcat(suite->complete_name, suite->parent->complete_name);
+                strcat(suite->complete_name, TEST_SEPARATOR);
+            }
+
+            strcat(suite->complete_name, suite->name);
+
+            if (!array_is_empty (suite->test_cases)) {
+                test_report_suite(suite->complete_name);
+            }
+        }
+
+        if (!array_is_empty(suite->test_cases)) {
+            struct ArrayIterator it = array_it_make(suite->test_cases);
+            struct TestCase * test;
+            while(!array_it_end(it)) {
+                test = array_it_get(it);
+                test_report_case(test, test->test());
+                it = array_it_next(it);
+            }
+            printf("\n");
+        }
+    }
+
+    test_suite_it_destroy(suite_it);
+
+    gettimeofday(&t2, NULL);
+    long elapsed = (t2.tv_sec - t1.tv_sec) * 1e6 + (t2.tv_usec - t1.tv_usec);
+
+    test_report_results(seed, elapsed);
+    test_suite_destroy(root);
+    
+    if (number_failed) {
+        return 1;
+    }
+    return 0;
+}
