0730dde7974e66251ba569502ae8a51f3cfa7755
Abel Calluaud <abel0b@protonmail.com>
2022-05-21 00:21:52 +0200

    Update build configuration

--- extern/inut/premake5.lua
+++ extern/inut/premake5.lua
@@ -1,16 +1,8 @@
-project "inut"
-    kind "StaticLib"
-    language "C"
-if os.host() == "windows" then
-    defines { "WINDOWS=1", "_CRT_SECURE_NO_WARNINGS", "_CRT_NONSTDC_NO_WARNINGS" }
-else
-    defines { "UNIX=1", "_POSIX_C_SOURCE=200809L" }
-end
-    includedirs { "include" }
-    files { "include/**.h", "inut/**.h", "inut/**.c" }
-
-
-function libinut()
-   links "inut"
-end
-
+workspace "inut"
+    configurations { "debug", "release" }
+    if os.host() == "windows" then
+        defines { "WINDOWS=1", "_CRT_SECURE_NO_WARNINGS", "_CRT_NONSTDC_NO_WARNINGS" }
+    else
+        defines { "UNIX=1", "_POSIX_C_SOURCE=200809L" }
+    end
+    require "inut.lua"
