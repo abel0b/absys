88a8c87018041f18e2ffb2d36f481decd762bb86
Abel Calluaud <abel0b@protonmail.com>
2022-05-20 20:10:37 +0200

    Support windows

--- extern/inut/premake5.lua
+++ extern/inut/premake5.lua
@@ -3,3 +3,8 @@ project "inut"
     language "C"
     includedirs { "include" }
     files { "include/**.h", "inut/**.h", "inut/**.c" }
+if os.host() == "windows" then
+    defines { "WINDOWS=1", "_CRT_SECURE_NO_WARNINGS", "_CRT_NONSTDC_NO_WARNINGS" }
+else
+    defines { "UNIX=1", "_POSIX_C_SOURCE=200809L" }
+end
